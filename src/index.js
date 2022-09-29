8bddefc1748078c317c36be75d14a63822133352
Jacqueline Falcone <jackiefalcone44@gmail.com>
2022-09-29 17:05:33 -0400

    Added source code link at the bottom

--- src/index.js
+++ src/index.js
@@ -14,13 +14,6 @@ function formatDay(timestamp) {
   let day = days[dayIndex];
   return `${day} ${hours}:${minutes}`;
 }
-// function formatDay(timestamp) {
-//   let date = new Date(timestamp * 1000);
-//   let day = date.getDay();
-//   let days = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
-
-//   return days[day];
-// }
 
 function displayForecast(response) {
   let forecast = response.data.daily;
