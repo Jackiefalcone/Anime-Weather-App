ba03ffa67ae3986604a977bfd823dcb12b34121e
Jacqueline Falcone <jackiefalcone44@gmail.com>
2022-09-30 11:09:48 -0400

    added to html button type "button" and deleted type"submit"

--- src/index.js
+++ src/index.js
@@ -113,9 +113,18 @@ function displayCelsiusTemperature(event) {
   celsiusLink.classList.add("active");
   fahrenheitLink.classList.remove("active");
 }
+function searchLocation(position) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
+
+  //position.coords.latitude
+  //position.coords.longitude
+}
 function getCurrentLocation(event) {
   event.preventDefault();
-  navigator.geolocation.getCurrentPosition(search);
+  navigator.geolocation.getCurrentPosition(searchLocation);
 }
 let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
